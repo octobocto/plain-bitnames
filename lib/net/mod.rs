b6d09826d85104ea2256b2dc6fcf68e6848918cf
octobocto <bo@jalborg.com>
2026-09-08 22:12:05 -0700

    net: add the alphanet seed node

--- lib/net/mod.rs
+++ lib/net/mod.rs
@@ -156,6 +156,15 @@ const SIGNET_SEED_NODE_ADDRS: &[SocketAddr] = {
     &[SIGNET_MINING_SERVER, BIP300_XYZ]
 };
 
+const ALPHANET_SEED_NODE_ADDRS: &[SocketAddr] = {
+    // The alphanet server runs a node for this chain.
+    const ALPHANET_SERVER: SocketAddr = SocketAddr::new(
+        std::net::IpAddr::V4(std::net::Ipv4Addr::new(204, 168, 254, 113)),
+        4000 + THIS_SIDECHAIN as u16,
+    );
+    &[ALPHANET_SERVER]
+};
+
 const FORKNET_SEED_NODE_ADDRS: &[SocketAddr] = {
     // explorer.bip300.xyz
     const BIP300_XYZ: SocketAddr = SocketAddr::new(
@@ -167,8 +176,7 @@ const FORKNET_SEED_NODE_ADDRS: &[SocketAddr] = {
 
 const fn seed_node_addrs(network: Network) -> &'static [SocketAddr] {
     match network {
-        // No seed runs on alphanet yet.
-        Network::Alphanet => &[],
+        Network::Alphanet => ALPHANET_SEED_NODE_ADDRS,
         Network::Signet => SIGNET_SEED_NODE_ADDRS,
         Network::Regtest => &[],
         Network::Forknet => FORKNET_SEED_NODE_ADDRS,
