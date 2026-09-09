3d04ee8b263a4371a9c09d413fa4108e9e9e932c
octobocto <bo@jalborg.com>
2026-09-08 20:47:54 -0700

    Node: maintain tip in active mainchain

--- lib/node/mod.rs
+++ lib/node/mod.rs
@@ -91,10 +91,10 @@ where
             env_open_opts
                 .map_size(128 * 1024 * 1024 * 1024) // 128 GB
                 .max_dbs(
-                    State::NUM_DBS
-                        + Archive::NUM_DBS
+                    Archive::NUM_DBS
                         + MemPool::NUM_DBS
-                        + Net::NUM_DBS,
+                        + Net::NUM_DBS
+                        + State::NUM_DBS,
                 );
             // Apply LMDB "fast" flags consistent with our benchmark setup:
             // - WRITE_MAP lets us write directly into the memory map instead of
@@ -124,7 +124,7 @@ where
         let zmq_pub_handler = Arc::new(ZmqPubHandler::new(zmq_addr).await?);
         let archive = Archive::new(&env)?;
         let mempool = MemPool::new(&env)?;
-        let (mainchain_task, mainchain_task_response_rx) =
+        let (mainchain_task, mainchain_task_event_rx) =
             MainchainTaskHandle::new(
                 env.clone(),
                 archive.clone(),
@@ -145,7 +145,7 @@ where
             env.clone(),
             archive.clone(),
             mainchain_task.clone(),
-            mainchain_task_response_rx,
+            mainchain_task_event_rx,
             mempool.clone(),
             net.clone(),
             peer_info_rx,
@@ -726,16 +726,18 @@ where
         // Check BMM
         {
             let rotxn = self.env.read_txn()?;
-            if self.archive.get_bmm_result(
+            match self.archive.get_bmm_result(
                 &rotxn,
                 block_hash,
                 main_block_hash,
-            )? == BmmResult::Failed
-            {
-                tracing::error!(%block_hash,
-                    "Rejecting block {block_hash} due to failing BMM verification",
-                );
-                return Ok(false);
+            )? {
+                BmmResult::Verified => (),
+                BmmResult::Failed => {
+                    tracing::error!(%block_hash,
+                        "Rejecting block {block_hash} due to failing BMM verification",
+                    );
+                    return Ok(false);
+                }
             }
         }
         // Check that ancestor bodies exist, and store body
