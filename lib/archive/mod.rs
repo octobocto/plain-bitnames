de8b7cd1b70c242a11f9597a7d1977271a916b48
octobocto <bo@jalborg.com>
2026-09-11 08:56:21 -0700

    Archive: move error to lib/archive/error.rs

--- lib/archive/mod.rs
+++ lib/archive/mod.rs
@@ -1,16 +1,13 @@
 use std::{
     cmp::Ordering,
     collections::{BTreeMap, HashMap, HashSet, VecDeque},
-    path::PathBuf,
 };
 
 use bitcoin::{self, hashes::Hash as _};
 use fallible_iterator::{FallibleIterator, IteratorExt};
 use heed::types::SerdeBincode;
 use sneed::{
-    DatabaseUnique, EnvError, RoTxn, RwTxn, RwTxnError, UnitKey,
-    db::{self, error::Error as DbError},
-    env, rwtxn,
+    DatabaseUnique, RoTxn, RwTxn, UnitKey, db::error::Error as DbError,
 };
 
 use crate::types::{
@@ -22,84 +19,8 @@ pub mod iter;
 pub use iter::{AncestorHeaders, Ancestors};
 pub mod side_tips;
 pub use side_tips::SideTips;
-
-#[allow(clippy::duplicated_attributes)]
-#[derive(Debug, thiserror::Error, transitive::Transitive)]
-#[transitive(
-    from(db::error::Delete, DbError),
-    from(db::error::Get, DbError),
-    from(db::error::Last, DbError),
-    from(db::error::Put, DbError),
-    from(db::error::TryGet, DbError),
-    from(env::error::CreateDb, EnvError),
-    from(env::error::WriteTxn, EnvError),
-    from(rwtxn::error::Commit, RwTxnError),
-    from(side_tips::error::DisconnectMainchainTip, side_tips::Error),
-    from(side_tips::error::DisconnectSidechainTip, side_tips::Error)
-)]
-pub enum Error {
-    #[error(transparent)]
-    Db(Box<DbError>),
-    #[error("Database env error")]
-    DbEnv(#[from] EnvError),
-    #[error("Database write error")]
-    DbWrite(#[from] RwTxnError),
-    #[error(
-        "Incompatible DB version ({}). Please clear the DB (`{}`) and re-sync",
-        .version,
-        .db_path.display()
-    )]
-    IncompatibleVersion { version: Version, db_path: PathBuf },
-    #[error("invalid previous side hash")]
-    InvalidPrevSideHash,
-    #[error("invalid merkle root")]
-    InvalidMerkleRoot,
-    #[error("no ancestor with depth {depth} for block {block_hash}")]
-    NoAncestor { block_hash: BlockHash, depth: u32 },
-    #[error("no block with hash {0}")]
-    NoBlock(BlockHash),
-    #[error("unknown block hash: {0}")]
-    NoBlockHash(BlockHash),
-    #[error("no BMM result with block {0}")]
-    NoBmmResult(BlockHash),
-    #[error("no block body with hash {0}")]
-    NoBody(BlockHash),
-    #[error("no deposits info for block {0}")]
-    NoDepositsInfo(bitcoin::BlockHash),
-    #[error("no header with hash {0}")]
-    NoHeader(BlockHash),
-    #[error("no height info for block hash {0}")]
-    NoHeight(BlockHash),
-    #[error("no mainchain ancestor with depth {depth} for block {block_hash}")]
-    NoMainAncestor {
-        block_hash: bitcoin::BlockHash,
-        depth: u32,
-    },
-    #[error("unknown mainchain block hash: {0}")]
-    NoMainBlockHash(bitcoin::BlockHash),
-    #[error("no mainchain block info for block hash {0}")]
-    NoMainBlockInfo(bitcoin::BlockHash),
-    #[error("no mainchain header info for block hash {0}")]
-    NoMainHeaderInfo(bitcoin::BlockHash),
-    #[error("no height info for mainchain block hash {0}")]
-    NoMainHeight(bitcoin::BlockHash),
-    #[error("no tx with txid {0}")]
-    NoTx(Txid),
-    #[error(transparent)]
-    SideTips(Box<side_tips::Error>),
-}
-
-impl From<DbError> for Error {
-    fn from(err: DbError) -> Self {
-        Self::Db(Box::new(err))
-    }
-}
-
-impl From<side_tips::Error> for Error {
-    fn from(err: side_tips::Error) -> Self {
-        Self::SideTips(Box::new(err))
-    }
-}
+pub mod error;
+pub use error::Error;
 
 #[derive(Clone)]
 pub struct Archive {
