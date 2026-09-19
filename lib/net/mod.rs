624dc772b55a92f8c01db4268f6a7fd13b72295e
octobocto <bo@jalborg.com>
2026-09-19 06:48:22 -0700

    net: seed betanet from seed.beta.ecash.eu.com

--- lib/net/mod.rs
+++ lib/net/mod.rs
@@ -167,16 +167,11 @@ const SIGNET_SEED_PEER_ADDRS: &[PeerAddress<&'static str>] = {
 };
 
 const BETANET_SEED_PEER_ADDRS: &[PeerAddress<&'static str>] = {
-    // The betanet servers run a node for this chain.
-    const DRIVECHA_IN: PeerAddress<&'static str> = PeerAddress {
-        host: url::Host::Domain("seed.beta.ecash.drivecha.in"),
+    const ECASH_EU_COM: PeerAddress<&'static str> = PeerAddress {
+        host: url::Host::Domain("seed.beta.ecash.eu.com"),
         port: DEFAULT_PORT,
     };
-    const ECASH_NINJA: PeerAddress<&'static str> = PeerAddress {
-        host: url::Host::Domain("seed.beta.ecash.ninja"),
-        port: DEFAULT_PORT,
-    };
-    &[DRIVECHA_IN, ECASH_NINJA]
+    &[ECASH_EU_COM]
 };
 
 const FORKNET_SEED_PEER_ADDRS: &[PeerAddress<&'static str>] = {
